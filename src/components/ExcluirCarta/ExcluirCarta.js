01ea98700b24c96a442b10591bd1caf714887d1f
Paulo Renato <paulorenatorf@hotmail.com>
2021-11-11 15:28:52 -0300

    correcao2

--- src/components/ExcluirCarta/ExcluirCarta.js
+++ src/components/ExcluirCarta/ExcluirCarta.js
@@ -37,8 +37,10 @@ const EditCarta = () => {
       alert("Essa carta nao pode ser excluida por causa do diferencial.");
       return;
     } else if (backCartas.length > 18) {
-      axios.delete(`/cartas/${id}`);
-      console.log("excluiu carta");
+      //axios.delete(`/cartas/${id}`);
+      api.delete(`/cartas/${id}`);
+      //console.log("excluiu carta");
+      //console.log(id);
       window.location.reload(false);
     } else {
       alert("O jogo precisa ter pelo menos 18 cartas.");
